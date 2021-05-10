2a3a97b1491d310b004a3bca460d1ee89f1b1b59
Kübra Köse <kubra@freelancer-dscience.tech>
2021-05-10 12:21:21 +0300

    Stok Çıkış Menüsünde BeyannameNo,Beyanname Tarih ve Tps No zorunluluğu kaldırıldı.İşlem tarihi günün tarihi atıldı.

--- Chep.WebUi/wwwroot/scripts/app/controllers/StokCikisController.js
+++ Chep.WebUi/wwwroot/scripts/app/controllers/StokCikisController.js
@@ -413,7 +413,10 @@
                                 text: "Ekle",
                                 icon: "add",
                                 onItemClick: function () {
+                                    var today = new Date();
                                     $scope.object.stokCikisId = 0;
+                                    //Bugünki Tarihi Alması için
+                                    $scope.object.islemTarihi = today.toISOString();
                                     $modalDetail.modal('show');
                                 }
                             });
@@ -487,91 +490,91 @@
                 focusedRowEnabled: true,
             }).dxDataGrid('instance');
         }
-        if ($modalDrop1 == null) {
-            $btnDropSubmit1 = $('#btnDropSubmit1');
+        //if ($modalDrop1 == null) {
+        //    $btnDropSubmit1 = $('#btnDropSubmit1');
 
-            $modalDrop1 = $('#modalDrop1').on({
-                "hide.bs.modal": function (e) {
-                    if ($('.modal:visible').length > 1) {
-                        bodyModalPadding = $('body').css('paddingRight');
-                    } else {
-                        bodyModalPadding = 0;
-                    }
-                },
-                "hidden.bs.modal": function (e) {
-                    $btnDropSubmit.prop('disabled', true);
-                    $scope.object.Drop = {};
+        //    $modalDrop1 = $('#modalDrop1').on({
+        //        "hide.bs.modal": function (e) {
+        //            if ($('.modal:visible').length > 1) {
+        //                bodyModalPadding = $('body').css('paddingRight');
+        //            } else {
+        //                bodyModalPadding = 0;
+        //            }
+        //        },
+        //        "hidden.bs.modal": function (e) {
+        //            $btnDropSubmit.prop('disabled', true);
+        //            $scope.object.Drop = {};
 
-                    $gridDrop.option("dataSource", []);
+        //            $gridDrop.option("dataSource", []);
 
-                    $modalDrop1.find('form .form-group').removeClass('has-success').removeClass('has-error');
-                    $modalDrop1.find('form .form-control').removeClass('ng-valid').removeClass('ng-invalid');
-                    $modalDrop1.find('form .input-icon .fa').removeClass('fa-check').removeClass('fa-warning');
+        //            $modalDrop1.find('form .form-group').removeClass('has-success').removeClass('has-error');
+        //            $modalDrop1.find('form .form-control').removeClass('ng-valid').removeClass('ng-invalid');
+        //            $modalDrop1.find('form .input-icon .fa').removeClass('fa-check').removeClass('fa-warning');
 
-                    $scope.$apply();
-                }
-            }).modal({
-                show: true,
-                keyboard: false,
-                backdrop: false
-            });
+        //            $scope.$apply();
+        //        }
+        //    }).modal({
+        //        show: true,
+        //        keyboard: false,
+        //        backdrop: false
+        //    });
 
-            //if ($gridDrop1 == null) {
-            //    $gridDrop1 = $("#gridDrop1").dxDataGrid({
-            //        keyExpr: "stokGirisDetayId",
-            //        dataSource: [],
-            //        columns: [
-            //            { dataField: "girisBeyannameNo", caption: "Beyanname No", width: 150, },
-            //            { dataField: "tpsNo", caption: "TPS No", width: 150, },
-            //            { dataField: "urunKod", caption: "Ürün Kodu", },
-            //            {
-            //                dataField: "girisMiktar", caption: "Giriş Adet", dataType: "number",
-            //                format: { type: "fixedPoint", precision: 0 },
-            //            },
-            //            {
-            //                dataField: "kalanMiktar", caption: "Kalan Adet", dataType: "number",
-            //                format: { type: "fixedPoint", precision: 0 },
-            //            },
-            //            {
-            //                dataField: "dusulenMiktar", caption: "Düşülen Adet", dataType: "number",
-            //                format: { type: "fixedPoint", precision: 0 },
-            //            },
-            //            {
-            //                dataField: "bakiyeMiktar", caption: "Bakiye Adet", dataType: "number",
-            //                format: { type: "fixedPoint", precision: 0 },
-            //            },
-            //        ],
-            //        summary: {
-            //            totalItems: [
-            //                {
-            //                    column: "dusulenMiktar",
-            //                    summaryType: "sum",
-            //                },
-            //                {
-            //                    column: "bakiyeMiktar",
-            //                    summaryType: "sum",
-            //                }
-            //            ]
-            //        },
-            //        onCellPrepared: function (e) {
-            //            if (e.rowType == "data" && e.data.DusulenMiktar > 0) {
-            //                $(e.cellElement).css('backgroundColor', '#e8f0fe');
-            //            }
-            //        },
-            //        filterRow: {
-            //            visible: false,
-            //        },
-            //        groupPanel: {
-            //            visible: false,
-            //        },
-            //        showBorders: true,
-            //        showRowLines: true,
-            //        sorting: {
-            //            mode: "none"
-            //        },
-            //    }).dxDataGrid('instance');
-            //}
-        }
+        //    //if ($gridDrop1 == null) {
+        //    //    $gridDrop1 = $("#gridDrop1").dxDataGrid({
+        //    //        keyExpr: "stokGirisDetayId",
+        //    //        dataSource: [],
+        //    //        columns: [
+        //    //            { dataField: "girisBeyannameNo", caption: "Beyanname No", width: 150, },
+        //    //            { dataField: "tpsNo", caption: "TPS No", width: 150, },
+        //    //            { dataField: "urunKod", caption: "Ürün Kodu", },
+        //    //            {
+        //    //                dataField: "girisMiktar", caption: "Giriş Adet", dataType: "number",
+        //    //                format: { type: "fixedPoint", precision: 0 },
+        //    //            },
+        //    //            {
+        //    //                dataField: "kalanMiktar", caption: "Kalan Adet", dataType: "number",
+        //    //                format: { type: "fixedPoint", precision: 0 },
+        //    //            },
+        //    //            {
+        //    //                dataField: "dusulenMiktar", caption: "Düşülen Adet", dataType: "number",
+        //    //                format: { type: "fixedPoint", precision: 0 },
+        //    //            },
+        //    //            {
+        //    //                dataField: "bakiyeMiktar", caption: "Bakiye Adet", dataType: "number",
+        //    //                format: { type: "fixedPoint", precision: 0 },
+        //    //            },
+        //    //        ],
+        //    //        summary: {
+        //    //            totalItems: [
+        //    //                {
+        //    //                    column: "dusulenMiktar",
+        //    //                    summaryType: "sum",
+        //    //                },
+        //    //                {
+        //    //                    column: "bakiyeMiktar",
+        //    //                    summaryType: "sum",
+        //    //                }
+        //    //            ]
+        //    //        },
+        //    //        onCellPrepared: function (e) {
+        //    //            if (e.rowType == "data" && e.data.DusulenMiktar > 0) {
+        //    //                $(e.cellElement).css('backgroundColor', '#e8f0fe');
+        //    //            }
+        //    //        },
+        //    //        filterRow: {
+        //    //            visible: false,
+        //    //        },
+        //    //        groupPanel: {
+        //    //            visible: false,
+        //    //        },
+        //    //        showBorders: true,
+        //    //        showRowLines: true,
+        //    //        sorting: {
+        //    //            mode: "none"
+        //    //        },
+        //    //    }).dxDataGrid('instance');
+        //    //}
+        //}
         //$gridContainer.beginCustomLoading();
 
         //ListData();
