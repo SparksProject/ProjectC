1d0e1d7b2b2b186ca1f72baecc2eabfd872ba3b7
Ahmed Burak <burak@freelancer-dscience.tech>
2021-08-02 15:06:31 +0300

    model güncellendi. schema compare edildi.

--- Chep.Database/dbo/Views/vw_WsWorkOrderInvoice.sql
+++ Chep.Database/dbo/Views/vw_WsWorkOrderInvoice.sql
@@ -1,11 +1,15 @@
-﻿create view [dbo].[vw_WsWorkOrderInvoice]
+﻿
+
+CREATE view [dbo].[vw_WsWorkOrderInvoice]
 as
 select 
-  cikis.StokCikisId,cikis.InvoiceId,cikis.WorkOrderMasterId,cikis.ReferansNo as WorkOrderNo,
-  '' as SenderNo,'' as SenderName,'' as SenderAddress,'' as SenderCity,'' as SenderCountry,'' as ConsgnNo
-  ,'' as ConsgnName,'' as ConsgnAddress,'' as ConsgnCity,'' as ConsgnCountry,'' as TransptrName,'' as VesselName
-  ,'' as AgentName,'' as PlateNo,'' as AwbNo,'' as BLNo,cikis.TeslimSekli as Incoterms,'' as DeliveryLocation
-  ,cikis.InvoiceAmount as InvoiceAmount,cikis.InvoiceCurrency as InvoiceCurrency,0 as FreightAmount,'' as FreightCurrency
-  ,0 as InsuranceAmount,'' as InsuranceCurrency,'' as ContainerNo,'' as GtbReferenceNo,cikis.OdemeSekli as PaymentMethod
-  ,cikis.CikisGumruk as EntryExitCustoms,'' as Customs
+  cikis.StokCikisId,cikis.InvoiceId,cikis.InvoiceId as WorkOrderMasterId,cikis.ReferansNo as WorkOrderNo,
+  gon.TaxNo as SenderNo,gon.Name as SenderName,gon.Adress as SenderAddress,gon.City as SenderCity,gon.Country as SenderCountry,ali.TaxNo as ConsgnNo
+  ,ali.Name as ConsgnName,ali.Adress as ConsgnAddress,ali.City as ConsgnCity,ali.Country as ConsgnCountry,(isnull(Nak.TaxNo,'') + ' '+ Nak.Name) as TransptrName,'' as VesselName
+  ,'' as AgentName,cikis.CikisAracKimligi as PlateNo,'' as AwbNo,'' as BLNo,cikis.TeslimSekli as Incoterms,'' as DeliveryLocation  ,cikis.InvoiceAmount as InvoiceAmount,
+  cikis.InvoiceCurrency as InvoiceCurrency,0 as FreightAmount,'' as FreightCurrency,0 as InsuranceAmount,'' as InsuranceCurrency,'' as ContainerNo,
+  cikis.GtbReferenceNo as GtbReferenceNo,cikis.OdemeSekli as PaymentMethod,cikis.CikisGumruk as EntryExitCustoms,cikis.CikisGumruk as Customs
 from ChepStokCikis cikis
+JOIN Customer Gon on cikis.IhracatciFirma=gon.CustomerId
+JOIN Customer Ali on cikis.AliciFirma=ali.CustomerId
+LEFT OUTER JOIN Customer Nak on cikis.NakliyeciFirma=Nak.CustomerId
